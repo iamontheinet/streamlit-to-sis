e466b1a2943292b052c79ba01767a653ad1dc4af
Dash Desai <iamontheinet@gmail.com>
2023-12-01 12:34:31 -0800

    Updated app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 # Setup web page
 st.set_page_config(
-    page_title="Streamlit to Streamlit-in-Snowflake",
+    page_title="Streamlit to Streamlit in Snowflake",
     layout="wide",
     menu_items={
         "Get Help": "https://developers.snowflake.com",
@@ -14,8 +14,8 @@ st.set_page_config(
     },
 )
 
-st.header(f"Streamlit to Streamlit in Snowflake (SiS)")
-st.subheader("See which features in your Streamlit app are currently not supported in SiS")
+st.header(f"Streamlit to Streamlit in Snowflake")
+st.subheader("See which features in your Streamlit app are currently not supported in Streamlit in Snowflake")
 st.caption(f"If you're new to Snowflake and Streamlit, checkout this step-by-step [QuickStart Guide](https://quickstarts.snowflake.com/guide/getting_started_with_snowpark_for_python_streamlit/index.html).")
 st.caption(f"App developed by Dash | [Twitter](https://twitter.com/iamontheinet) | [LinkedIn](https://www.linkedin.com/in/dash-desai/)")
 st.markdown("___")
@@ -25,31 +25,23 @@ docs_url = "https://docs.snowflake.com/developer-guide/streamlit/limitations#lab
 def unsupported_features(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    all_features = []
     features = []
-    possible_features = []
     others = []
-    features = soup.find_all("a", {"class": "reference external"})
+    all_features = soup.find_all("a", {"class": "reference external"})
 
     try:
-        features = [f.text for f in features if f.text.startswith("st.")]
+        for f in all_features:
+            if f.text.startswith("st."):
+                features.append(f.text)
+            else:
+               if f.text != "Third Party Terms":
+                others.append(f.text)
         features = features + [f.replace("st.", "st.sidebar.") for f in features]
     except:
         features += ['N/A']
-    
-    try:
-        possible_features = features + [f.replace("st.", ".") for f in features]
-    except:
-        possible_features = ['N/A']
-    
-    try:
-        others = soup.find("div", {"id": "limitations-and-unsupported-features-during-preview"}).find_all(
-            "div"
-        )
-        others = [o.find("h3").text[:-1] for o in others]
-    except:
-        others = ['N/A']
         
-    return features, possible_features, others
+    return features, others
 
 def check_valid_github_url(url):
     if "github" in url and ("blob" in url or "raw" in url):
@@ -77,7 +69,7 @@ with st.container():
         )
 
 if uploaded_file is not None or check_valid_github_url(streamlit_link):
-    features, possible_features, others = unsupported_features(docs_url)
+    features, others = unsupported_features(docs_url)
     if uploaded_file is not None:
         code_lines = uploaded_file.getvalue().decode("utf-8").splitlines()
     elif check_valid_github_url(streamlit_link):
@@ -88,35 +80,20 @@ if uploaded_file is not None or check_valid_github_url(streamlit_link):
     if code_lines:
         with st.container():
             bad_features = []
-            possible_bad_features = []
-            for code_line in code_lines:
+            for line_number, code_line in enumerate(code_lines, start=1):
                 for f in features:
+                    # print(f)
                     idx = code_line.find(f)
                     if idx > 0:
                         bad_features.append([f, code_line.strip()])
-                for p in possible_features:
-                    idx = code_line.find(p)
-                    if idx > 0:
-                        possible_bad_features.append([p, code_line.strip()])
 
-            if not (bad_features and possible_bad_features):
+            if not (bad_features):
                 st.snow()
-                st.markdown("""<h1 style='color:#29b5e8; text-align:center'>Congratulations! Your Streamlit app is ready to be ported to SiS!</h1>""",unsafe_allow_html=True)          
+                st.markdown("""<h1 style='color:#29b5e8; text-align:center'>Congratulations! Your Streamlit app is ready to be ported to Snowflake!</h1>""",unsafe_allow_html=True)          
             else:
-                bdf = pd.DataFrame(bad_features, columns=["Unsupported Feature in SiS", "Line"])
-                pbf = pd.DataFrame(
-                    possible_bad_features, columns=["Possibly Unsupported Feature in SiS", "Line"]
-                )
-
-                # Remove any lines from pbf where the Line is in bdf
-                pbf = pbf[~pbf["Line"].isin(bdf["Line"])]
-
+                bdf = pd.DataFrame(bad_features, columns=["Unsupported Feature", "Code"]).set_index("Unsupported Feature")
                 st.subheader("Unsupported Features")
                 st.dataframe(bdf, use_container_width=True)
-
-                st.markdown("___")
-                st.subheader("Possibly Unsupported Features")
-                st.dataframe(pbf, use_container_width=True)   
 
             st.markdown("___")
 
